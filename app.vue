aec9d249d1304371f94ae560ee236d6145b524e5
Hannes Schacherl <h.scha.hs@gmail.com>
2024-08-01 20:39:08 +0200

    remove font & make dark color theme default

--- app.vue
+++ app.vue
@@ -1,37 +1,19 @@
 <template>
-  <div class="overflow-x-hidden judson-regular overflow-y-auto">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://fonts.googleapis.com/css2?family=Judson:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">
+  <div class="overflow-x-hidden overflow-y-auto">
     <NuxtPage />
   </div>
 </template>
 
 <script setup lang="ts">
 onMounted(() => {
-  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-        document.documentElement.classList.add('dark');
+    if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.remove('dark');
     } else {
-        document.documentElement.classList.remove('dark')
+        document.documentElement.classList.add('dark');
     }
 });
 </script>
 
 <style>
-.judson-regular {
-  font-family: "Judson", serif;
-  font-weight: 400;
-  font-style: normal;
-}
 
-.judson-bold {
-  font-family: "Judson", serif;
-  font-weight: 700;
-  font-style: normal;
-}
-
-.judson-regular-italic {
-  font-family: "Judson", serif;
-  font-weight: 400;
-  font-style: italic;
-}
 </style>
